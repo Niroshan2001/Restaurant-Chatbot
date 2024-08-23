99b6370b624cd7e168d83ba12957456001ac2f60
Niroshan2001 <2001niroshan123@gmail.com>
2024-08-23 15:17:07 +0530

    update about and contact

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
-# import streamlit as st
-
-# st.title('Srilankan Taste')
-# st.header('Welcome to Srilankan Taste')
-
-# st.info('Information about Srilankan Taste')
-
-# st.write('This is a web application that provides information about Srilankan Taste.')
-
-# st.markdown('## Description')
-
-
-
-# st.checkbox('I agree')
-
-# st.button('Submit')
-
-# st.select_slider('Select a value', options=['A', 'B', 'C'])
-
 import streamlit as st
 from PIL import Image
 import requests
@@ -40,21 +21,18 @@ def load_and_resize_image(image_path, size=(200, 200)):
     img = img.resize(size)
     return img
 
-st.markdown("""
-    <style>
-    /* CSS to increase the size of the tab labels */
-    div[data-testid="stHorizontalBlock"] div[role="tab"] {
-        font-size: 60px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+
 # Create tabs for navigation
+st.title("Serandib Spice House")
 tabs = st.tabs(["Home", "About", "Contact Us"])
+
+
 
 # Home Page Tab
 with tabs[0]:
-    st.title("Restaurant Menu Dashboard")
-
+    
+    st.image("main.jpg", use_column_width=True)
+    st.title("Menu")
     # Display the menu items in rows of three
     for i in range(0, len(menu_data), 3):
         cols = st.columns(3)  # Create three columns
@@ -68,19 +46,41 @@ with tabs[0]:
 
 # About Page Tab
 with tabs[1]:
-    st.title("About Us")
-    st.write("""
-        Welcome to our restaurant! We pride ourselves on serving the best quality food made with fresh ingredients.
-        Whether you're in the mood for pizza, burgers, or something healthier like a salad, we have something for everyone.
-        Visit us and experience great service and delicious food.
-    """)
+    st.markdown("<h1 style='text-align: center;'>About Us</h1>", unsafe_allow_html=True)
+    col1, col2 = st.columns([3,2])
+    
+    with col1:
+        
+        st.header("Welcome to Serendib Spice House")
+        st.write(
+           """
+            Established in 1998, Serendib Spice House has been a cornerstone of Colombo's dining scene, merging the rich culinary traditions of South and North India. Our restaurant offers a diverse menu that includes both vegetarian and non-vegetarian dishes, catering to a wide range of preferences.
+
+            Our philosophy is rooted in using only the freshest ingredients and authentic spices to craft our flavorful dishes. Whether you're looking for vegan delights or indulgent non-veg options, our menu is designed to offer something for everyone. 
+
+            Over the years, we have cultivated a reputation for excellence, thanks to our commitment to high standards in both food quality and service. Our loyal customers, many of whom have been with us since the beginning, are a testament to the enduring appeal of our cuisine.
+
+            At Serendib Spice House, we strive to create a warm and inviting atmosphere where every meal is a celebration of tradition and taste. We invite you to experience our unique blend of flavors and enjoy a dining experience that has been cherished by many for decades.
+        """)
+    with col2:
+        st.image("main.jpg", use_column_width=True)
 
 # Contact Us Page Tab
 with tabs[2]:
-    st.title("Contact Us")
-    st.write("""
+    st.markdown("<h1 style='text-align: center;'>Contact Us</h1>", unsafe_allow_html=True)
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.write("""
         We'd love to hear from you! Please reach out to us with any questions or feedback.
-        \n**Phone**: +1 (234) 567-890
-        \n**Email**: info@restaurant.com
-        \n**Address**: 123 Food Street, Tasty Town, Flavorland
+            
+            Phone: +94 11 123 4567
+            Email: serendib@restaurant.com
+            Address: No. 5,Fedrica Road,Wellawatte, Colombo 00600, Sri Lanka
+                 
+        Operating Hours:
+            Monday to Friday: 11:00 AM - 10:00 PM
+            Saturday and Sunday: 12:00 PM - 11:00 PM
     """)
+    with col2:
+        st.image("map.png", use_column_width=True)
+    
